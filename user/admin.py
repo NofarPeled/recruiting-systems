b34318d09a0bbfc225f1074d9d655d6ef8e68fdc
NofarPeled <nofarpeled4@gmail.com>
2021-08-11 21:16:44 +0300

    created - custom user model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from user.models import User
 
-# Register your models here.
+from django.contrib.auth.models import Group
+
+admin.site.unregister(Group)
+
+class UserAdmin(admin.ModelAdmin):
+    fields=['first_name','last_name', 'email', 'language',]
+    readonly_fields = ('updated_at', 'created_at',)
+    ordering=('id',)
+    
+
+admin.site.register(User)
